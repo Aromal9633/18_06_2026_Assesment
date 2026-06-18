87651a68fb97a525a371390f7aeb0fad6a170521
Aromal9633 <299546@ust.com>
2026-06-18 12:36:18 +0530

    commit

--- List.py
+++ List.py
@@ -1,3 +1,45 @@
 
-List1=[1,2,3,4,5,6,7]
-print(List1)
+#Basic List operations
+
+
+lst = [10, 20, 30]
+
+lst.append(40)
+lst.insert(1, 15)
+lst.remove(20)
+
+for i in lst:
+    print(i)
+
+
+
+#Perform List Manipulation
+
+lst = [10, 20, 30, 40, 50]
+
+print("Original List:", lst)
+
+lst.append(60)
+print("After Append:", lst)
+
+lst.insert(2, 25)
+print("After Insert:", lst)
+
+lst.remove(30)
+print("After Remove:", lst)
+
+lst.pop()
+print("After Pop:", lst)
+
+lst.reverse()
+print("After Reverse:", lst)
+
+lst.sort()
+print("After Sort:", lst)
+
+print("Length of List:", len(lst))
+
+print("Elements in List:")
+for i in lst:
+    print(i)
+
