14d57b0e29786c1e1ed0206aa4d255d524cd374e
Aromal9633 <299546@ust.com>
2026-06-18 18:23:26 +0530

    Completed Python exercises

---
+++ Sets.py
@@ -0,0 +1,37 @@
+#Perform Basic Set Operations
+
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+print(set1)
+print(set1.union(set2))
+print(set1.intersection(set2))
+print(set1.difference(set2))
+print(set1.symmetric_difference(set2))
+
+
+#Union of Sets
+
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+print(set1.union(set2))
+
+#Intersection of Sets
+
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+print(set1.intersection(set2))
+
+#Difference of Sets
+
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+print(set1.difference(set2))
+print(set2.difference(set1))
+
+
+
+
