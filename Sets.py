3bcdce3f042830066cf9c6155d9552133d9575f3
Aromal9633 <299546@ust.com>
2026-06-19 10:25:08 +0530

    Completed Python exercises

--- Sets.py
+++ Sets.py
@@ -32,6 +32,21 @@ set2 = {3, 4, 5, 6}
 print(set1.difference(set2))
 print(set2.difference(set1))
 
+#Find Common Elements in Two Lists
+
+set1=[1,2,3,4,5]
+set2=[2,3,8,1,9]
+common_sets=set(set1 ) & set(set2)
+print("Common elements in th List is:",common_sets)
 
 
+#Count Unique Words
+
+words1="Welcome to Python Training Python first day"
+count=words1.split()
+unique1=set(count)
+uni_list=len(unique1)
+
+print(unique1)
+print("count of unique words are:",uni_list)
 
