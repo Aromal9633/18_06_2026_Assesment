69846628b9636d25a7da9714abf42d59c98e26c5
Aromal9633 <299546@ust.com>
2026-06-23 15:31:07 +0530

    Update Sets.py

--- Sets.py
+++ Sets.py
@@ -1,104 +1,149 @@
-#Perform Basic Set Operations
+                            #Perform Basic set operations in Python
 
-set1 = {1, 2, 3, 4}
-set2 = {3, 4, 5, 6}
+s={1,2,3,4,5,6,7}
+print("Items in the set is:",s)
 
-print(set1)
-print(set1.union(set2))
-print(set1.intersection(set2))
-print(set1.difference(set2))
-print(set1.symmetric_difference(set2))
+#Adding elements to Sets
 
+s={1,2,3,4}
+s.add(5)
 
-#Union of Sets
+print(s)
 
-set1 = {1, 2, 3, 4}
-set2 = {3, 4, 5, 6}
-
-print(set1.union(set2))
-
-#Intersection of Sets
-
-set1 = {1, 2, 3, 4}
-set2 = {3, 4, 5, 6}
-
-print(set1.intersection(set2))
-
-#Difference of Sets
-
-set1 = {1, 2, 3, 4}
-set2 = {3, 4, 5, 6}
-
-print(set1.difference(set2))
-print(set2.difference(set1))
-
-#Find Common Elements in Two Lists
-
-set1=[1,2,3,4,5]
-set2=[2,3,8,1,9]
-common_sets=set(set1 ) & set(set2)
-print("Common elements in th List is:",common_sets)
-
-#Remove Items From Set Simultaneously
-
-
-s = {1, 2, 3, 4, 5}
-remove_items = {2, 4}
-s.difference_update(remove_items)
+#Updating Sets
+s={1,2,3,4}
+s.update([5,6,7])
 print(s)
 
 
+#removing elements in Sets
+s={1,2,3,4,5,6}
+s.remove(6)
+print(s)
 
-#Count Unique Words
 
-words1="Welcome to Python Training Python first day"
-count=words1.split()
-unique1=set(count)
-uni_list=len(unique1)
+#pop
 
-print(unique1)
-print("count of unique words are:",uni_list)
+s={1,2,3,4,5,6}
+s.pop()
+print(s)
+
+
+#popitem
+s={1,2,3,4,5,6}
+s.clear()
+print("Output is :",s)
+
+
+                               #SET OPERATIONS
+#Union operations in set
+a={1,2,3,4}
+b={5,6,7,8}
+print(a.union(b))
+
+#Intersection
+a={1,2,3,4}
+b={5,6,7,8,3,4}
+print("Intersection Output:",a.intersection(b))
+
+#Difference
+a={1,2,3,4}
+b={5,6,7,8}
+print("Difference",b-a)
+
+#Remove duplicates
+
+
+nums = [1, 2, 2, 3, 4, 4]
+unique_nums = set(nums)
+print(unique_nums)
+
+#Symetric Difference
+
+a={1,2,3,4}
+b={2,3,4,5}
+print(a^b)
+print(a.symmetric_difference(b))
+
+#Add list of elements to a set
+
+a={1,2}
+lst=[3,4,5,6]
+a.update(lst)
+print(a)
+
+#Set Differnce update
+
+a={1,2,7,8}
+b={5,6,7,8}
+
+a.difference_update(b)
+print("Set Difference Update is:",a)
+
+
+#Remove Items from Sets Simultenoulsy
+
+a={1,2,3,4,5,6,7,8,9,10}
+a.difference_update({5,6,7})
+#a.remove(5)
+print(a)
+
+
+#Check Subset
+
+a={1,2,3}
+b={1,2,3,4,5,6,7}
+print(a.issubset(b))
+
+#Check superset
+
+a={1,2,3,4,5,6,7}
+b={1,2,3}
+print(a.issuperset(b))
+ 
+
+#Check Intersection
+
+a={1,2,3,7}
+b={1,2,3,4,5,6,7}
+print(a.intersection(b))
+
+#Set Symmetric Difference Update
+
+a={1,2,3,4,5,6,7}
+b={1,2,3}
+a.symmetric_difference_update(b)
+print("Symmetric Difference",a)
+
 
 #Set Intersection Update
 
 
-set1 = {1, 2, 3, 4}
-set2 = {3, 4, 5, 6}
-print(set1.intersection(set2))
-set1.intersection_update(set2)
-print(set1)
+a = {1, 2, 3}
+b = {1, 2, 3, 4, 5, 6, 7}
+a.intersection_update(b)
+print("Intersection Update",a)
 
-#Set Symmetric Difference Update
+#Find Common elements in two lists
 
-
-set1 = {1, 2, 3, 4}
-set2 = {3, 4, 5, 6}
-set1.symmetric_difference_update(set2)
-print(set1)
-
-
-#Set Difference Update
-
-
-s = {1, 2, 3, 4, 5}
-remove_items = {2, 4}
-s.difference_update(remove_items)
-print(s)
+a={1,2,3,4,5}
+b={5,6,7,4,10,11,12}
+c=a&b
+print(c)
 
 #Frozen Set
 
-fs = frozenset([1, 2, 3, 4])
+fs=frozenset([1,2,3,4,5,6])
 print(fs)
 
 #Count Unique Words
 
-
 text = "python is easy and python is powerful"
-
-words = text.split()
-print(words)
-unique_words = set(words)
-
-print(len(unique_words))
+words=text.split()
+unique=set(words)
+count=len(unique)
+print("Count of Unique Words is:",count)
 
 
+a={1,2,1,2,3,2,1,3,4,5,6}
+print(set(a))
